a02e15ad2f4d13ec9f17095d1247cde05166d46a
Utsav sharma <utsavsharma697@gmail.com>
2020-07-26 14:53:26 +0530

    map modification

--- Atithi/Components/plan.js
+++ Atithi/Components/plan.js
@@ -1,3 +1,5 @@
+
+
 import React, { Component } from 'react';
 import {
     StyleSheet,
@@ -5,27 +7,93 @@ import {
     View,
     TextInput,
     TouchableOpacity,
-    StatusBar
+    StatusBar,
+    Platform,
+    Alert
 } from 'react-native';
 
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+import Geolocation from '@react-native-community/geolocation';
+
+Geolocation.setRNConfiguration(config);
+import {reqest, PERMISSIONS} from 'react-native-permissions'
 
 class Plan extends Component {
+
+
+    componentDidMount() {
+        this.requestLocationPermission();
+    }
+
+    
+    requestLocationPermission = async () => {
+        if(Platform.OS === 'ios'){
+            var response = await reqest(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+            console.log('iPhone: ' + response);
+
+
+            if(response === 'granted'){
+                this.locateCurrentPosition();
+            }
+        }
+        else{
+            var response =await request(PERMISSIONS.ANDROID.LOCATION_WHEN_IN_USE)
+            console.log('Android: ' + response);
+
+            if(response === 'granted'){
+                this.locateCurrentPosition();
+            }
+        }
+
+    }
+
+
+    locateCurrentPosition = () => {
+        Geolocation.getCurrentPosition(
+            position => {
+                console.log(JSON.stringify(position));
+
+                let initialPosition = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.035
+                }
+
+                this.setState({initialPosition})
+
+            },
+            error => Alert.alert(error.message),
+            {enableHighAccuracy: true, timeout: 10000, maximumAge: 1000}
+        )
+    }
+
+
+
+
     render() {
         return (
 
             <View style={styles.container}>
                 <MapView
-                
+
+                ref = {map => this._map = map }
+
+
                 style={styles.map}
-                region={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-                }}
+                initialRegion={this.state.initialPosition}
                 >
+                
+                <Marker
+                    coordinate={{latitude: 37.78825,
+                    longitude: -122.4324}}
+                    title={"My firrst marker"} />
+                
                 </MapView>
+
+                
+                   
         
             </View>
 
